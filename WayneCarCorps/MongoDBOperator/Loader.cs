6293aba0546be9c4e55468ed8f709acf852c1b06
YovkoGospodinov <yovko.gospodinov@gmail.com>
2016-10-31 19:26:42 +0200

    adding mongoDB loader

---
+++ WayneCarCorps/MongoDBOperator/Loader.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using WayneCarCorps.MongoDBModels.Interfaces;
+
+namespace MongoDBOperator
+{
+    public class Loader<T>
+    {
+        private Connector connector = new Connector();
+        private IMongoDatabase database;
+        private IMongoClient client;
+
+        public Loader()
+        {
+            client = connector.Connect();
+            database = connector.GetDatabbase(client);
+        }
+
+        public void LoadEntities(IEnumerable<T> collectionToInsert, string collectionName)
+        {
+            var collection = database.GetCollection<T>(collectionName);
+
+            collection.InsertMany(collectionToInsert);
+        }
+    }
+}
